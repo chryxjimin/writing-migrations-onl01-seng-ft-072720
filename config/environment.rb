5e6f0359fb5104dcc8700e5783fb6484ca2e8e14
Christie Yoon <cleex1999@gmail.com>
2020-09-01 20:07:30 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -13,11 +13,6 @@ if ENV["SCHOOL_ENV"] == "test"
   ActiveRecord::Migration.verbose = false
 end
 
-ActiveRecord::Base.establish_connection(
-  :adapter => "sqlite3",
-  :database => "db/students.sqlite"
-)
-
 
 def drop_db
   DB.tables.each do |table|
